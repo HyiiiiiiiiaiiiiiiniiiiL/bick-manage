9251ed12202f3536e2167279acbee3fe79744dcd
HiiYiiLii <892630853@qq.com>
2019-03-06 19:51:45 +0800

    bike manage init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import { Button } from 'antd'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Button>点击一下</Button>
+
+      </div>
+    );
+  }
+}
+
+export default App;
