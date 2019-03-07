a7d436f104f23f291ec73a30aa5d665d6525c55f
HiiYiiLii <892630853@qq.com>
2019-03-07 17:56:21 +0800

    使用Row定义页面架构

---
+++ src/components/NavLeft/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Menu, Icon } from 'antd'
+import MenuConfig from "./../../config/menuConfig"
+const SubMenu = Menu.SubMenu
+export default class NavLeft extends React.Component {
+    render() {
+        return <div>
+            <div className="logo">
+                <img src="" alt="" />
+                <h1>Imooc  MS</h1>
+            </div>
+        </div>
+    }
+}
