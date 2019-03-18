ef37c7d6c63066211772ab5ae2c9e03248da5344
HiiYiiLii <892630853@qq.com>
2019-03-18 18:51:59 +0800

    定义中间内容

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import "./index.less"
 
 export default class Footer extends React.Component {
     render() {
-        return <div>
-            footer
+        return <div className="footer">
+            版权归属肉啵特股份有限制公司，技术支持：@肉啵特
         </div>
     }
 }
