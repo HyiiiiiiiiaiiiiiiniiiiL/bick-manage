455a28d8069a9e6cdbe62f5e3427c9be99f60f62
HiiYiiLii <892630853@qq.com>
2019-03-22 13:56:55 +0800

    动态路由

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
-import Admin from "./pages/demo/router-demo/router02/Router"
+import Admin from "./pages/demo/router-demo/router03/Router"
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Admin />, document.getElementById('root'));
