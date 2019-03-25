5b2efde6c041af63808772eb3a7b7d75d6852ff0
HiiYiiLii <892630853@qq.com>
2019-03-25 14:01:21 +0800

    全局提示组件

--- src/router.js
+++ src/router.js
@@ -7,6 +7,7 @@ import Buttons from './pages/ui/buttons'
 import Modals from './pages/ui/modals'
 import Loading from './pages/ui/loadings'
 import Notice from './pages/ui/notice'
+import Message from './pages/ui/Message'
 import NoMatch from "./pages/nomatch"
 export default class Router extends React.Component {
     render() {
@@ -23,6 +24,7 @@ export default class Router extends React.Component {
                                 <Route path="/admin/ui/modals" component={Modals} />
                                 <Route path="/admin/ui/loadings" component={Loading} />
                                 <Route path="/admin/ui/notice" component={Notice} />
+                                <Route path="/admin/ui/message" component={Message} />
                                 <Route component={NoMatch} />
                             </Switch>
                         </Admin>
