2b7396cc4881bb1257ec7cc1688c09987c34ae8d
HiiYiiLii <892630853@qq.com>
2019-03-29 17:38:24 +0800

    heighTable初始化

---
+++ src/pages/table/heighTable.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Card, Table, Modal, Button, message } from "antd"
+import axios from "../../axios"
+import Utils from '../../Utils/utils';
+
+export default class HeighTable extends React.Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
